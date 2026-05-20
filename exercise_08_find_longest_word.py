e048643eabe2e340e6d5d160a218b5656c815491
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-05-20 19:40:20 -0300

    Update exercise_08_find_longest_word.py

--- exercise_08_find_longest_word.py
+++ exercise_08_find_longest_word.py
@@ -27,4 +27,19 @@ def find_longest_word(filename):
         # archivo contiene: "el gato corre rapido\npor el jardin\n"
         find_longest_word("texto.txt") -> "rapido"
     """
-    pass  # Reemplazar con tu implementación
+    with open(filename, 'r') as archivo:
+        contenido = archivo.read()
+
+    palabras = contenido.split()
+
+    if len(palabras) == 0:
+        raise ValueError("file has no words")
+
+    palabra_mas_larga = palabras[0]
+
+    for palabra in palabras:
+        if len(palabra) > len(palabra_mas_larga):
+            palabra_mas_larga = palabra
+
+    return palabra_mas_larga
+
