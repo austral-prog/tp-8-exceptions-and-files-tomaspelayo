5df654caa0e75e80481fbefb6e610b7bee6cc18e
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-05-20 19:41:58 -0300

    Update exercise_09_merge_files.py

--- exercise_09_merge_files.py
+++ exercise_09_merge_files.py
@@ -32,4 +32,12 @@ def merge_files(file1, file2, output):
         # hola
         # mundo
     """
-    pass  # Reemplazar con tu implementación
+    with open(file1, 'r') as f1:
+        contenido1 = f1.read()
+
+    with open(file2, 'r') as f2:
+        contenido2 = f2.read()
+
+    with open(output, 'w') as f_out:
+        f_out.write(contenido1 + contenido2)
+
